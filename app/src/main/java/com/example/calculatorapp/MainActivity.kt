7070a19bc9255bff8897c2a575ccb3273feaf32e
Surbhi <surbhi150404@gmail.com>
2024-04-27 23:02:05 +0530

    updated splash screen
    updated splash screen

--- app/src/main/java/com/example/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -9,89 +9,101 @@ class MainActivity : AppCompatActivity() {
  lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        binding= ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
 
         binding.btnAC.setOnClickListener {
-            binding.tvSolution.text=""
-            binding.tvResult.text=""
+            binding.tvSolution.text = ""
+            binding.tvResult.text = ""
         }
 
-        binding.btn0.setOnClickListener {
-            binding.tvSolution.append("0")
-        }
 
-        binding.btn1.setOnClickListener {
-            binding.tvSolution.append("1")
-        }
 
-        binding.btn2.setOnClickListener {
-            binding.tvSolution.append("2")
-        }
+            binding.btn0.setOnClickListener {
+                binding.tvSolution.append("0")
+            }
 
-        binding.btn3.setOnClickListener {
-            binding.tvSolution.append("3")
-        }
-        binding.btn4.setOnClickListener {
-            binding.tvSolution.append("4")
-        }
+            binding.btn1.setOnClickListener {
+                binding.tvSolution.append("1")
+            }
 
-        binding.btn5.setOnClickListener {
-            binding.tvSolution.append("5")
-        }
+            binding.btn2.setOnClickListener {
+                binding.tvSolution.append("2")
+            }
 
-        binding.btn6.setOnClickListener {
-            binding.tvSolution.append("6")
-        }
+            binding.btn3.setOnClickListener {
+                binding.tvSolution.append("3")
+            }
+            binding.btn4.setOnClickListener {
+                binding.tvSolution.append("4")
+            }
 
-        binding.btn7.setOnClickListener {
-            binding.tvSolution.append("7")
-        }
+            binding.btn5.setOnClickListener {
+                binding.tvSolution.append("5")
+            }
 
-        binding.btn8.setOnClickListener {
-            binding.tvSolution.append("8")
-        }
+            binding.btn6.setOnClickListener {
+                binding.tvSolution.append("6")
+            }
 
-        binding.btn9.setOnClickListener {
-            binding.tvSolution.append("9")
-        }
+            binding.btn7.setOnClickListener {
+                binding.tvSolution.append("7")
+            }
 
-        binding.btnDot.setOnClickListener {
-            binding.tvSolution.append(".")
-        }
+            binding.btn8.setOnClickListener {
+                binding.tvSolution.append("8")
+            }
 
-        binding.btnAdd.setOnClickListener {
-            binding.tvSolution.append("+")
-        }
-        binding.btnDivide.setOnClickListener {
-            binding.tvSolution.append("/")
-        }
-        binding.btnMinus.setOnClickListener {
-            binding.tvSolution.append("-")
-        }
-        binding.btnMultiply.setOnClickListener {
-            binding.tvSolution.append("*")
-        }
-        binding.btnOpenBracket.setOnClickListener {
-            binding.tvSolution.append("(")
-        }
-        binding.btnCloseBracket.setOnClickListener {
-            binding.tvSolution.append(")")
-        }
+            binding.btn9.setOnClickListener {
+                binding.tvSolution.append("9")
+            }
 
-        binding.btnEqual.setOnClickListener {
-            val expression= ExpressionBuilder(binding.tvSolution.text.toString()).build()
-            val result= expression.evaluate()
-            val longResult= result.toLong()
+            binding.btnDot.setOnClickListener {
+                binding.tvSolution.append(".")
+            }
 
-            if (result==longResult.toDouble()){
-            binding.tvResult.text= longResult.toString()
+            binding.btnAdd.setOnClickListener {
+                binding.tvSolution.append("+")
+            }
+            binding.btnDivide.setOnClickListener {
+                binding.tvSolution.append("/")
+            }
+            binding.btnMinus.setOnClickListener {
+                binding.tvSolution.append("-")
+            }
+            binding.btnMultiply.setOnClickListener {
+                binding.tvSolution.append("*")
+            }
+            binding.btnOpenBracket.setOnClickListener {
+                binding.tvSolution.append("(")
+            }
+            binding.btnCloseBracket.setOnClickListener {
+                binding.tvSolution.append(")")
+            }
+
+            binding.btnEqual.setOnClickListener {
+                val expression = ExpressionBuilder(binding.tvSolution.text.toString()).build()
+                val result = expression.evaluate()
+                val longResult = result.toLong()
+
+                if (result == longResult.toDouble()) {
+                    binding.tvResult.text = longResult.toString()
+                } else {
+                    binding.tvResult.text = result.toString()
+                }
+            }
+        binding.btnC.setOnClickListener {
+
+            val currentText = binding.tvSolution.text.toString()
+            if (currentText.isNotEmpty()) {
+                // Remove the last character from the input
+                val newText = currentText.substring(0, currentText.length - 1)
+                binding.tvSolution.text = newText
             }
             else{
-                binding.tvResult.text= result.toString()
-            }
+                binding.tvSolution.text.toString()
+            }}
         }
     }
-}
